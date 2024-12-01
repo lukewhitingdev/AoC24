21a930b75178ca510a0b5d29cef3a9ece4708bae
Luke Whiting <43708922+lukewhitingdev@users.noreply.github.com>
2024-12-01 15:20:02 +0000

    Update Program.cs

--- 1/src/Program.cs
+++ 1/src/Program.cs
@@ -7,11 +7,24 @@ public class Program
 {
     private static void Main(string[] args)
     {
+        // First test the base case.
+        int[] left = new int[]{3, 4, 2, 1, 3, 3};
+        int[] right = new int[]{4, 3, 5, 3, 9, 3};
 
+        RunTest(left, right, 11);
     }
 
-    public static void DoWork()
+    private static void RunTest(in int[] left, in int[] right, int expected_dist)
     {
-        
+        int found = Dist(left, right);
+        if(found != expected_dist)
+        {
+            throw new Exception("Failed test");
+        }
+    }
+
+    public static int Dist(in int[] left, in int[] right)
+    {
+        return -1;
     }
 }
