913ee7ad75c602f1e107b947132016bb56e33986
Luke Whiting <43708922+lukewhitingdev@users.noreply.github.com>
2024-12-01 15:15:47 +0000

    Added devcontainer and setup C# for day 1

---
+++ 1/src/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace AoC;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+
+    }
+
+    public static void DoWork()
+    {
+        
+    }
+}
